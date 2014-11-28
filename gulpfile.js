a4815ff96b959e1805e801bf124f353e311e91eb
Ollie Relph <ollie@relph.me>
2014-11-28 17:29:14 +0000

    Compile the JS & CSS to a dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
   gulp.src('./public/stylesheets/canvas_game.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./public/stylesheets/'));
+    .pipe(gulp.dest('./public/dist/'));
 });
 
-gulp.task('watch', function () {
+gulp.task('js', function () {
+  gulp.src([
+    './public/javascripts/structures.js',
+    './public/javascripts/controls.js',
+    './public/javascripts/explosion.js',
+    './public/javascripts/snowball.js',
+    './public/javascripts/player.js',
+    './public/javascripts/camera.js',
+    './public/javascripts/game.js',
+    './public/javascripts/snowman_game.js',
+  ])
+  .pipe(concat('snowman-game.js'))
+  .pipe(gulp.dest('./public/dist/'));
+});
+
+gulp.task('watch', ['sass', 'js'], function () {
   gulp.watch('./public/stylesheets/*', ['sass']);
+  gulp.watch('./public/javascripts/*', ['js']);
 });
