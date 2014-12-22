7105ca5275b664e223562d12f4f7d3224813f6c1
Ollie Relph <ollie@relph.me>
2014-12-22 14:55:23 +0000

    Remove unused files

--- routes/games.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/snowman', function(req, res) {
-  res.render('snowman');
-});
-
-module.exports = router;
